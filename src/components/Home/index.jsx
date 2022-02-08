161d03f448e3423a700f3502175cff60bf01befa
Patrick Grady <patrickrgrady81@gmail.com>
2022-02-08 07:14:01 -0500

    hooked up login button to onclick

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,10 +5,15 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const Home = () => {
+
+    const handleLogIn = () => {
+        console.log("Clicked")
+    }
+
     return (
         <div className="home-wrapper">
             <div className="btn-wrapper">
-            <Button className="login-btn">Login</Button>
+            <Button className="login-btn" onClick={handleLogIn}>Login</Button>
             <Button className="admin-btn">Admin</Button>
             </div>
         </div>
